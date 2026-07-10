fd06d4d8d11e8d9fba4ed4f32cd1c4aef80e020f
Namballa-Srivansh <srivansh90@gmail.com>
2026-07-10 20:21:55 +0530

    Added Starter Evaluation service

---
+++ Backend/EvaluationService/src/config/redis.config.ts
@@ -0,0 +1,22 @@
+import Redis from "ioredis";
+import logger from "./logger.config";
+
+const redisConfig = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT) || 6379,
+    maxRetriesPerRequest: null,
+}
+
+export const redis = new Redis(redisConfig);
+
+redis.on("connect", () => {
+    logger.info("Connected to redis successfully")
+});
+
+redis.on("error", (error) => {
+    logger.error("failed to connect to redis", error);
+});
+
+export const createNewRedisConnection = () => {
+    return new Redis(redisConfig);
+}
